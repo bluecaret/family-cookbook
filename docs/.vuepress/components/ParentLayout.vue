4df584e38c73527e9b22d114a4a56089db3a7345
bluecaret <dev@bluecaret.com>
2025-06-06 10:43:36 -0700

    Redirect to new cookbook site

--- docs/.vuepress/components/ParentLayout.vue
+++ docs/.vuepress/components/ParentLayout.vue
@@ -1,6 +1,7 @@
 <script setup>
-import TimerWidget from './TimerWidget.vue'
-import '../styles/index.scss'
+import TimerWidget from "./TimerWidget.vue";
+import "../styles/index.scss";
+window.location.href = "https://cookedcaret.netlify.app/";
 </script>
 
 <template>
@@ -36,7 +37,11 @@ import '../styles/index.scss'
           <span>Tag</span>
         </a>
         <a :href="$withBase('/extras')">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
+            />
+          </svg>
           <span>Extras</span>
         </a>
       </nav>
@@ -48,7 +53,10 @@ import '../styles/index.scss'
   <footer class="footer">
     <div class="footerInner container">
       <TimerWidget class="timer" />
-      <div class="createdBy">Family Cookbook created by <a href="https://www.bluecaret.com">BlueCaret</a>.</div>
+      <div class="createdBy">
+        Family Cookbook created by
+        <a href="https://www.bluecaret.com">BlueCaret</a>.
+      </div>
       <a href="#" class="top">▲</a>
     </div>
   </footer>
